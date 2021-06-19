378efce0daee2623f12e02a2b9755ea7a6546370
Tejas Dhanda <tejas11personal@gmail.com>
2021-06-19 14:34:50 +0530

    new functions.py file with and only useful data returner. no change in main.py(non compatible with functions.py)

---
+++ functions.py
@@ -0,0 +1,83 @@
+import youtube_dl as you
+
+
+def YoutubeDL(url):
+    y = you.YoutubeDL()
+    r = y.extract_info(url=url, download=False)
+    return r
+# print(YoutubeDL('https://www.youtube.com/playlist?list=PLCOOUY9uAnn82EnBxCXpN6uF9pYVTvBqQ'))
+
+# playlist url
+# https://www.youtube.com/playlist?list=PLCOOUY9uAnn82EnBxCXpN6uF9pYVTvBqQ
+
+
+
+def playlistRawDataModifier(playlistUrl):
+    playlistRawData = YoutubeDL(playlistUrl)
+    mapDataToBeReturned ={}
+    # title playlist
+    title_playlist = playlistRawData['title']
+    print('title_playlist = '+ str(title_playlist))
+    mapDataToBeReturned['title'] = title_playlist
+    mapDataToBeReturned['video'] = {}
+
+    # individual video stuff
+
+    listIndividualVideos = []
+
+    entriesInPlaylist = playlistRawData['entries']
+    numEntryVideo = -1
+    for eachEntry in entriesInPlaylist:
+        print('eachEntry = '+str(eachEntry))
+        numEntryVideo += 1
+        # print(eachEntry)
+        # video title
+        returnabletitleVideo = eachEntry['title']
+        print('titleVideo '+ str(returnabletitleVideo))
+        # link highest quality
+        listOfFormats = eachEntry['formats']
+        # print('listOFFFFFFFFFF' +str(listOfFormats))
+        curentHighestQualityVid = {}
+        curentHighestQualityAud = {}
+
+        # getting to know best format datas
+        for eachFormatNum in range(len(listOfFormats)):
+            currentFormat = listOfFormats[eachFormatNum]
+            # VIDEO
+            if currentFormat['vcodec'] != 'none' and currentFormat['acodec'] == 'none':
+                curentHighestQualityVid[str(eachFormatNum)] = int(currentFormat['format_note'][:-1])
+            # Audio
+            if currentFormat['acodec'] != 'none' and currentFormat['vcodec'] == 'none':
+                # print(str(eachFormatNum)+'/n/n/n/n/n/n/n/n/n'+str(currentFormat)+'/n/n/n/n/n/n/n/n/n/n/n')
+
+                curentHighestQualityAud[str(eachFormatNum)] = float(currentFormat['tbr'])
+
+        sortedcurentHighestQualityVid_List = sorted(
+            curentHighestQualityVid.items(),
+            key=
+            lambda qual: -qual[1]
+        )
+        sortedcurentHighestQualityAud_List = sorted(
+            curentHighestQualityAud.items(),
+            key=
+            lambda qual: -qual[1]
+        )
+        bestAudFormatNumber = int(sortedcurentHighestQualityAud_List[0][0])
+        bestVidFormatNumber = int(sortedcurentHighestQualityVid_List[0][0])
+
+        returnableVidURL = listOfFormats[bestVidFormatNumber]['url']
+        returnableAudURL = listOfFormats[bestAudFormatNumber]['url']
+
+        mapDataToBeReturned['video'][numEntryVideo] = {
+            'titleVid':(returnabletitleVideo),
+            'URLVid': (returnableVidURL),
+            'URLAud':(returnableAudURL)
+        }
+        # print(sortedcurentHighestQualityVid_List)
+        # print(bestVidFormatNumber)
+        # print(sortedcurentHighestQualityAud_List)
+        # print(bestAudFormatNumber)
+    print(mapDataToBeReturned)
+    return (mapDataToBeReturned)
+# playlistRawDataModifier(YoutubeDL('https://www.youtube.com/playlist?list=PLCOOUY9uAnn82EnBxCXpN6uF9pYVTvBqQ'))
+
